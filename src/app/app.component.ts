e3e873fdaf7410e278595ba6e52e8a8234a0484e
Bari Ademi <baroademi@gmail.com>
2024-03-11 21:53:55 +0100

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,23 @@
-import {Component} from '@angular/core';
+import {OnInit, Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Menu';
   Lang = ['AL', 'MK', 'EN']
-  lang='AL'
+  lang = 'AL'
 
 
-  langChanged(lang:string) {
-    localStorage.setItem('lang',lang)
+  langChanged(lang: string) {
+    localStorage.setItem('lang', lang)
     location.reload()
+  }
+
+  ngOnInit() {
+    this.langChanged(this.lang)
   }
 }
 
