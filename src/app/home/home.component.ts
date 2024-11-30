8564054944ae8ae5cd711233c1428b80ed45dcbc
Bari Ademi <baroademi@gmail.com>
2024-11-30 16:47:30 +0100

    loader

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {DataServiceService} from "../services/data-service.service";
 import {ScheduleModel} from "../models/schedule.model";
 
@@ -8,16 +8,33 @@ import {ScheduleModel} from "../models/schedule.model";
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  Schedule:ScheduleModel[]=[];
+  Schedule: ScheduleModel[] = [];
+  isFirstPhotoLoaded: boolean = false
+  isSecondPhotoLoaded: boolean = false
+  isThirdPhotoLoaded: boolean = false
 
-  constructor(private service:DataServiceService) { }
+  constructor(private service: DataServiceService) {
+  }
 
   ngOnInit(): void {
-  this.getWorkingHours();
+    this.getWorkingHours();
   }
 
 
-  getWorkingHours(){
+  getWorkingHours() {
     this.Schedule = this.service.getSchedule('AL')
+  }
+
+  photoIsLoaded(whichPhoto: number) {
+    switch (whichPhoto) {
+      case 1:
+        this.isFirstPhotoLoaded = true
+        break;
+      case 2:
+        this.isSecondPhotoLoaded = true
+        break;
+      case 3:
+        this.isThirdPhotoLoaded=true
+    }
   }
 }
